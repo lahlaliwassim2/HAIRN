fdea37fabc4c898c3d55fc97e7c597ee84a883fd
lahlaliwassim2 <wlahlali34@gmail.com>
2022-06-22 00:41:20 +0100

    affichier les données de la base de donnée

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -24,14 +24,14 @@
         <div class="container">
             <div class="row g-5  align-items-center">
                 <div class="col-lg-6">
-                    <h6 class="section-title text-start text-primary text-uppercase">About Us</h6>
-                    <h1 class="mb-4">Welcome to <span class="text-primary text-uppercase">BarberShop</span></h1>
+                    <h6 class="section-title text-start text-info text-uppercase">About Us</h6>
+                    <h1 class="mb-4">Welcome to <span class="text-info text-uppercase">BarberShop</span></h1>
                     <p class="mb-4">Fondé en 2022, The Barber Shop Group a un engagement passionné envers les soins pour hommes et propose des coupes de cheveux, coiffure  et rasage pour hommes de haute qualité et d'un bon rapport qualité-prix.</p>
                     <div class="row g-3 pb-4">
                         <div class="col-sm-4 wow fadeIn" data-wow-delay="0.1s">
                             <div class="border rounded p-1">
                                 <div class="border rounded text-center p-4">
-                                    <i class="fa fa-hotel fa-2x text-primary mb-2"></i>
+                                    <i class="fa fa-hotel fa-2x text-info mb-2"></i>
                                     <h2 class="mb-1" data-toggle="counter-up">1234</h2>
                                     <p class="mb-0">coupes</p>
                                 </div>
@@ -40,7 +40,7 @@
                         <div class="col-sm-4 wow fadeIn" data-wow-delay="0.3s">
                             <div class="border rounded p-1">
                                 <div class="border rounded text-center p-4">
-                                    <i class="fa fa-users-cog fa-2x text-primary mb-2"></i>
+                                    <i class="fa fa-users-cog fa-2x text-info mb-2"></i>
                                     <h2 class="mb-1" data-toggle="counter-up">1234</h2>
                                     <p class="mb-0">coiffeurs</p>
                                 </div>
@@ -49,17 +49,17 @@
                         <div class="col-sm-4 wow fadeIn" data-wow-delay="0.5s">
                             <div class="border rounded p-1">
                                 <div class="border rounded text-center p-4">
-                                    <i class="fa fa-users fa-2x text-primary mb-2"></i>
+                                    <i class="fa fa-users fa-2x text-info mb-2"></i>
                                     <h2 class="mb-1" data-toggle="counter-up">1234</h2>
                                     <p class="mb-0">BARBERS</p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <a class="btn btn-primary py-3 px-5 mt-2" href="">Voir plus</a>
+                    <a class="btn btn-info py-3 px-5 mt-2" href="">Voir plus</a>
                 </div>
                 <div class="col-lg-6">
-                    <div class="row g-3">
+                    <div class="row">
                         <div class="col-6 text-end">
                             <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.1s" src="<?php echo URLROOT ?>/img/about-1.jpg" alt="">
                         </div>
@@ -85,8 +85,8 @@
   <div class="container-xxl py-5">
     <div class="container">
         <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-            <h6 class="section-title text-center text-primary text-uppercase">notre coiffeurs</h6>
-            <h1 class="mb-5">Découvrez notre <span class="text-primary text-uppercase">coifeurs</span></h1>
+            <h6 class="section-title text-center text-info text-uppercase">notre coiffeurs</h6>
+            <h1 class="mb-5">Découvrez notre <span class="text-info text-uppercase">coifeurs</span></h1>
         </div>
         <div class="row g-4">
             <?php foreach($data as $coiffeur): ?>
@@ -95,9 +95,9 @@
                     <div class="position-relative">
                         <img class="img-fluid" src="<?php echo URLROOT ?>/img/team-1.jpg" alt="">
                         <div class="position-absolute start-50 top-100 translate-middle d-flex align-items-center">
-                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-square btn-info mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square btn-info mx-1" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square btn-info mx-1" href=""><i class="fab fa-instagram"></i></a>
                         </div>
                     </div>
                     <div class="text-center p-4 mt-3">
