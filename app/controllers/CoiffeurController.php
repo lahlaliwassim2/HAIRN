129769ab6974de2719c9a37c126ed175be03e91b
lahlaliwassim2 <wlahlali34@gmail.com>
2022-06-22 15:08:21 +0100

    ajouter la fonction d affichage et afichier les données dans des diferentes page

--- app/controllers/CoiffeurController.php
+++ app/controllers/CoiffeurController.php
@@ -18,21 +18,60 @@ class CoiffeurController extends Controller {
 
     public function addCoiffeur()
     {
+            $img = $this->checkImg();
             if($_SERVER['REQUEST_METHOD'] == 'POST'){
-                if(empty($_POST['name'])&&(empty($_POST['description']))){
+                if(empty($_POST['nom'])&&(empty($_POST['prenom']))&&(empty($_POST['nombre']))(empty($_POST['date']))){
                     redirect('pages/ajouterCoiffeur');
                 }    
                 else{
                 $data = $_POST;
-                $products = $this->CoiffeurModel->addCoiffeur($data);
-                redirect('pages/adminCoiffeur');
+                $coiffeurs = $this->CoiffeurModel->addCoiffeur($data,$img);
+                // redirect('pages/adminBarbers');
                 }
             }
             else{
                     redirect('pages/ajouterCoiffeur');
             }       
     }
+
+
+
+
+
+
+public function checkImg(){ 
+    $fileName = $_FILES["img"]["name"];
+    $fileSize = $_FILES["img"]["size"];
+    $tmpName = $_FILES["img"]["tmp_name"];
+
+    $validImageExtension = ['jpg', 'jpeg', 'png'];
+    $imageExtension = explode('.', $fileName);
+    $imageExtension = strtolower(end($imageExtension));
+    if ( !in_array($imageExtension, $validImageExtension) ){
+      echo
+      "
+      <script>
+        alert('Invalid Image Extension');
+      </script>
+      ";
+    }
+    else if($fileSize > 1000000){
+      echo
+      "
+      <script>
+        alert('Image Size Is Too Large');
+      </script>
+      ";
+    }
+    else{
+      $newImageName = uniqid();
+      $newImageName .= '.' . $imageExtension;
+      $folder = $_SERVER['DOCUMENT_ROOT'].'/barber-fr-mvc/public/upload/';
+      move_uploaded_file($tmpName,$folder. $newImageName);
+      return $newImageName;
+
+  }
 }
 
 
-
+}
